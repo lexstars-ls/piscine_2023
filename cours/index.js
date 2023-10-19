5ecbc28f98f95048ad1cf80fc1f60f98f164bf1c
Sisovath lucas <lucas.sisovath@egs.school>
2023-10-19 14:11:52 +0200

    add js cours

---
+++ cours/index.js
@@ -0,0 +1,49 @@
+///Présentaion des opérateurs arthmétiques
+
+// var myNumber1 = 5+5
+// var myNumber2 = 5
+// var myResult1 = myNumber1 * myNumber2  
+
+// console.log(myResult1)  (* / - % +)
+
+
+
+// Déclaration moderne (en ES6, depuis 2015) de variable
+
+
+//const
+
+// const myNb1 = 9
+
+// const myNb2 = 3
+
+// const res = myNb1 * myNb2
+
+// console.log(res)
+
+
+// let permissif
+// const myNb1 = 9
+// let myNb2 = 2
+// myNb2 +=1
+// const res2 = myNb1 * myNb2
+// console.log(res2)
+
+// LES CHAINES DE CARACTERES (string) '' "" ``
+
+// let myName = `je m'apelle "Paul"`
+// const Lastname  = "Doazan"
+// const age = 35
+// myName += ' ' + Lastname
+// myName +=', ' + age + 'ans.'
+// console.log(myName)
+
+// les BOOLEENS TRUE OR FALSE
+
+const myBoolean1 = true
+const myBoolean2 = false
+
+if(myBoolean1){
+    console.log()
+}
+console.log( 'end script')
